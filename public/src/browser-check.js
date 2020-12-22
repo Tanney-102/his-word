fbfb29990c66e9ea752cacccf972f9f9257dbae9
Tanney-102 <dhxodms89@gmail.com>
2020-12-22 13:35:51 +0900

    .

--- public/src/browser-check.js
+++ public/src/browser-check.js
@@ -2,16 +2,21 @@ export default function checkBrowser() {
   const userAgent = navigator.userAgent.toLowerCase();
   if (userAgent.search("kakaotalk") > -1) {
     if (userAgent.search("android") > -1) {
-      document.getElementById("app-container").style.display = "none";
       location.href=`intent://his-word.web.app#Intent;scheme=http;package=com.android.chrome;end`;
       window.downloadBtn = 1;
     } else {
       window.downloadBtn = 0;
+      document.getElementById("loading-page").style.display = "none";
+      document.getElementById("app-container").style.display = "block";
     }
   } else if (userAgent.search("iphone") > -1 || userAgent.search("ipad") > -1 || userAgent.search("ipod") > -1) {
     window.downloadBtn = 0;
+    document.getElementById("loading-page").style.display = "none";
+    document.getElementById("app-container").style.display = "block";
   } else {
     window.downloadBtn = 1;
+    document.getElementById("loading-page").style.display = "none";
+    document.getElementById("app-container").style.display = "block";
   }
 };
 
