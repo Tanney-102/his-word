93830c9ae37dc1acf0bc6730df6a8bf87a46cf09
Tanney-102 <dhxodms89@gmail.com>
2020-12-22 13:46:23 +0900

    .

--- public/src/browser-check.js
+++ public/src/browser-check.js
@@ -17,6 +17,10 @@ export default function checkBrowser() {
     window.downloadBtn = 1;
     document.getElementById("loading-page").style.display = "none";
     document.getElementById("app-container").style.display = "block";
+
+    if (userAgent.search("msie") > -1 ) {
+      alert("권장하지 않는 브라우저입니다. 다른 브라우저를 이용해주세요.(Chrome브라우저 권장)");
+    }
   }
 };
 
